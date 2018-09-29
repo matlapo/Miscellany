b385c789cf5e7222ab69c325e7aa02fc3a29e1fe
matlapo <mathieu.lapointe3@mail.mcgill.ca>
2018-09-29 17:38:58 -0400

    Top-down approach

--- MinimumPathSum.fs
+++ MinimumPathSum.fs
@@ -1,30 +1,66 @@
 open System
-
 let grid = [
     [1; 2; 3]
     [1; 1; 3]
     [3; 1; 1]
+    [1; 2; 1]
 ]
 
-let M = 2
+let M = 3
 let N = 2
 
-// not safe
+// not safe, but for simplicity
 let grid_val m n =
-    // printfn "m = %A n = %A" m n
     grid
     |> List.item m
     |> List.item n 
 
+let mutable naive_calls = 0
+let mutable dp_calls = 0
+
+// naive recursive solution
 let rec naive s =
+    naive_calls <- naive_calls + 1
     match s with
     | (0, 0) -> grid_val 0 0
     | (0, n) -> naive (0, n-1) + grid_val 0 n
     | (m, 0) -> naive (m-1, 0)+ grid_val m 0
-    | (m, n) ->
-        // if m = M && n = 0 then grid_val 1 0
-        // elif n = N && m = 0 then grid_val 0 1
-        // else 
-        let test = Math.Min (naive (m-1, n), naive (m, n-1)) + grid_val m n
-        printfn "for subprob %A %A the value is %A" m n test
-        Math.Min (naive (m-1, n), naive (m, n-1)) + grid_val m n
+    | (m, n) -> Math.Min (naive (m-1, n), naive (m, n-1)) + grid_val m n
+
+
+// slight modification of naive to make use of dynamic programming (top-down)
+// Issue: memory
+let top_down () = 
+    let mutable table = Map.empty
+    let rec helper (s: int * int) =
+        if table.ContainsKey s then 
+            table.Item s 
+        else
+            dp_calls <- dp_calls + 1
+            let value = 
+                match s with
+                | (0, 0) -> grid_val 0 0
+                | (0, n) -> helper (0, n-1) + grid_val 0 n
+                | (m, 0) -> helper (m-1, 0)+ grid_val m 0
+                | (m, n) -> Math.Min (helper (m-1, n), helper (m, n-1)) + grid_val m n
+            table <- table.Add (s, value)
+            value
+    helper (M, N)
+
+// in progress
+let bottom_up () = 
+    let mutable table = Map.empty
+    let rec helper (s: int * int) =
+        if table.ContainsKey s then 
+            table.Item s 
+        else
+            dp_calls <- dp_calls + 1
+            let value = 
+                match s with
+                | (0, n) -> helper (0, n-1) + grid_val 0 n
+                | (m, 0) -> helper (m-1, 0)+ grid_val m 0
+                | (m, n) -> Math.Min (helper (m+1, n), helper (m, n+1)) + grid_val m n
+            table <- table.Add (s, value)
+            value
+    table <- table.Add ((0, 0), (grid_val 0 0))
+    helper (0, 0)
