05d8ece8627ad260f63176bb8d5c354b02f7b677
matlapo <mathieu.lapointe3@mail.mcgill.ca>
2018-10-06 08:30:29 -0400

    Proper removeAtIndex

--- MinimumSum3NonOverlappingSubArrays.fs
+++ MinimumSum3NonOverlappingSubArrays.fs
@@ -1,13 +1,10 @@
-
-let removeAtIndex l i k =
-    let rec helper l current = 
-        match l with 
-        | [] -> []
-        | x::xs -> if current >= i && (current - i) <= k then helper xs (current + 1) else x::xs
-    helper l 0
+let removeAtIndex l i j =
+    l 
+    |> List.indexed
+    |> List.filter (fun (index, _) -> index < i || index > (i+j-1))
+    |> List.map snd
 
 let test = [1;2;1;2;6;7;5;1];;
 
-removeAtIndex test 3 4;;
+removeAtIndex test 3 2;;
 
-// let findLargest l k =
